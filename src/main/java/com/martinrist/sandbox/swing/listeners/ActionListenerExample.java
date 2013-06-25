11244ff9e1ac8f0295395f7a0e6a4ff2b6a65ed5
Martin Rist <mlr-github@martinrist.com>
2013-06-25 23:10:24 +0100

    Added first example of event handling

---
+++ src/main/java/com/martinrist/sandbox/swing/listeners/ActionListenerExample.java
@@ -0,0 +1,77 @@
+package com.martinrist.sandbox.swing.listeners;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import com.martinrist.sandbox.swing.DefaultFrame;
+
+public class ActionListenerExample {
+
+	public static void main(String[] args) {
+		
+		new ActionListenerExample().run();
+		
+	}
+
+	private void run() {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				ActionListenerFrame frame = new ActionListenerFrame("Button Example");
+				frame.setVisible(true);
+			}
+		});
+
+	}
+}
+
+
+class ActionListenerFrame extends DefaultFrame {
+	
+	private static final long serialVersionUID = 1L;
+
+	private JPanel buttonPanel = new JPanel();
+	
+	private JButton helloButton = new JButton("Say hello");
+	private JButton goodbyeButton = new JButton("Say goodbye");
+	private JLabel textLabel = new JLabel();
+	
+	public ActionListenerFrame(String name) {
+		super(name);
+		
+		helloButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				buttonPanel.setBackground(Color.GREEN);
+				goodbyeButton.setEnabled(true);
+				helloButton.setEnabled(false);
+				JOptionPane.showMessageDialog(ActionListenerFrame.this, "Hello!");
+			}
+		});
+
+		goodbyeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				buttonPanel.setBackground(Color.RED);
+				goodbyeButton.setEnabled(false);
+				helloButton.setEnabled(true);
+				JOptionPane.showMessageDialog(ActionListenerFrame.this, "Goodbye!");
+			}
+		});
+		
+		add(buttonPanel);
+		buttonPanel.add(helloButton);
+		buttonPanel.add(goodbyeButton);
+		buttonPanel.add(textLabel);
+		
+	}
+	
+}
