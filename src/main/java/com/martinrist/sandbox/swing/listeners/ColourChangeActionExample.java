ed98e08d5ba0004b828ecabbebd4045671445869
Martin Rist <mlr-github@martinrist.com>
2013-07-04 16:51:28 +0100

    Add icons to buttons

--- src/main/java/com/martinrist/sandbox/swing/listeners/ColourChangeActionExample.java
+++ src/main/java/com/martinrist/sandbox/swing/listeners/ColourChangeActionExample.java
@@ -2,10 +2,14 @@ package com.martinrist.sandbox.swing.listeners;
 
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.net.URL;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -26,8 +30,7 @@ public class ColourChangeActionExample {
 
 			@Override
 			public void run() {
-				Frame frame = new ColourChangeActionFrame(
-						"Colour Change Action Example");
+				Frame frame = new ColourChangeActionFrame("Colour Change Action Example");
 				frame.setVisible(true);
 			}
 		});
@@ -44,8 +47,7 @@ class ColourChangeActionFrame extends DefaultFrame {
 		super(title);
 
 		JButton redButton = new JButton(new ColourAction("Red", Color.RED));
-		JButton greenButton = new JButton(
-				new ColourAction("Green", Color.GREEN));
+		JButton greenButton = new JButton(new ColourAction("Green", Color.GREEN));
 		JButton blueButton = new JButton(new ColourAction("Blue", Color.BLUE));
 
 		add(panel);
@@ -64,6 +66,7 @@ class ColourChangeActionFrame extends DefaultFrame {
 			putValue(COLOUR_KEY, col);
 			putValue(Action.NAME, name);
 			putValue(Action.SHORT_DESCRIPTION, "Set colour to " + name);
+			putValue(Action.SMALL_ICON, createButtonIcon(name));
 		}
 
 		@Override
@@ -72,6 +75,13 @@ class ColourChangeActionFrame extends DefaultFrame {
 			panel.setBackground(c);
 		}
 
+		private Icon createButtonIcon(String name) {
+			URL iconUrl = this.getClass().getResource("/com/martinrist/sandbox/swing/listeners/" + name + ".png");
+			Image unscaledImage = new ImageIcon(iconUrl).getImage();
+			Image scaledImage = unscaledImage.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+			Icon scaledIcon = new ImageIcon(scaledImage);
+			return scaledIcon;
+		}
 	}
 
 }
