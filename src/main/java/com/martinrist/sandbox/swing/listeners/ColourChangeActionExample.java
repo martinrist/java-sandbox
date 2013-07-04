13b1e0031d2ec0e65795fbd0a955d332bea0d2ef
Martin Rist <mlr-github@martinrist.com>
2013-07-04 17:01:52 +0100

    Disable a button once it's been clicked

--- src/main/java/com/martinrist/sandbox/swing/listeners/ColourChangeActionExample.java
+++ src/main/java/com/martinrist/sandbox/swing/listeners/ColourChangeActionExample.java
@@ -5,6 +5,8 @@ import java.awt.Frame;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -43,12 +45,18 @@ class ColourChangeActionFrame extends DefaultFrame {
 	private static final long serialVersionUID = 1L;
 	private final JPanel panel = new JPanel();
 
+	List<JButton> buttons = new ArrayList<>();
+
 	public ColourChangeActionFrame(String title) {
 		super(title);
 
 		JButton redButton = new JButton(new ColourAction("Red", Color.RED));
 		JButton greenButton = new JButton(new ColourAction("Green", Color.GREEN));
 		JButton blueButton = new JButton(new ColourAction("Blue", Color.BLUE));
+
+		buttons.add(redButton);
+		buttons.add(greenButton);
+		buttons.add(blueButton);
 
 		add(panel);
 		panel.add(redButton);
@@ -73,6 +81,18 @@ class ColourChangeActionFrame extends DefaultFrame {
 		public void actionPerformed(ActionEvent e) {
 			Color c = (Color) getValue(COLOUR_KEY);
 			panel.setBackground(c);
+
+			JButton source = (JButton) e.getSource();
+			source.setEnabled(false);
+			enableOtherButtons(source);
+		}
+
+		private void enableOtherButtons(JButton clickedButton) {
+			for (JButton button : buttons) {
+				if (!button.equals(clickedButton)) {
+					button.setEnabled(true);
+				}
+			}
 		}
 
 		private Icon createButtonIcon(String name) {
