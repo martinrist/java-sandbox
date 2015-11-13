954b2de1bd2ce40a583db999c59c02ed1e7717a9
Martin Rist <mlr-github@martinrist.com>
2015-11-13 13:39:07 +0000

    Add initial Enum examples from TiJ Chapter 'Enumerated Types'.

---
+++ src/main/java/com/martinrist/thinkingInJava/enumerations/EnumClass.java
@@ -0,0 +1,22 @@
+package com.martinrist.thinkingInJava.enumerations;
+
+public class EnumClass {
+
+    public static void main(String[] args) {
+
+        for (Shrubbery s : Shrubbery.values()) {
+            System.out.println(s + " ordinal: " + s.ordinal());
+            System.out.println(s.compareTo(Shrubbery.CRAWLING));
+            System.out.println(s.equals(Shrubbery.CRAWLING));
+            System.out.println(s == Shrubbery.CRAWLING);
+            System.out.println(s.getDeclaringClass());
+            System.out.println(s.name());
+            System.out.println("--------------");
+        }
+    }
+}
+
+
+
+
+enum Shrubbery { GROUND, CRAWLING, HANGING }
