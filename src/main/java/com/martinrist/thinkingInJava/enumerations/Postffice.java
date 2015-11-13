06aa3ca599d65a043a630a7e07d7c1d3b4e262ee
Martin Rist <mlr-github@martinrist.com>
2015-11-13 15:17:47 +0000

    Example of using Enums for mail handling, from TiJ chapter 'Enumerated Types'

---
+++ src/main/java/com/martinrist/thinkingInJava/enumerations/Postffice.java
@@ -0,0 +1,84 @@
+package com.martinrist.thinkingInJava.enumerations;
+
+public class Postffice {
+
+    enum MailHandler {
+        GENERAL_DELIVERY {
+            boolean handle(Mail m) {
+                switch (m.generalDelivery) {
+                    case YES:
+                        System.out.println("Using general delivery for " + m);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        },
+        MACHINE_SCAN {
+            boolean handle(Mail m) {
+                switch (m.scannability) {
+                    case UNSCANNABLE:
+                        return false;
+                    default:
+                        switch (m.address) {
+                            case INCORRECT:
+                                return false;
+                            default:
+                                System.out.println("Delivering " + m + " automatically");
+                                return true;
+                        }
+                }
+            }
+        },
+        VISUAL_INSPECTION {
+            boolean handle(Mail m) {
+                switch (m.readability) {
+                    case ILLEGIBLE:
+                        return false;
+                    default:
+                        switch (m.address) {
+                            case INCORRECT:
+                                return false;
+                            default:
+                                System.out.println("Delivering " + m + " normally");
+                                return true;
+                        }
+                }
+            }
+        },
+        RETURN_TO_SENDER {
+            boolean handle(Mail m) {
+                switch (m.returnAddress) {
+                    case MISSING:
+                        return false;
+                    default:
+                        System.out.println("Returning " + m + " to sender");
+                        return true;
+                }
+            }
+        };
+
+        abstract boolean handle(Mail m);
+    }
+
+
+    static void handle(Mail m) {
+        for (MailHandler handler : MailHandler.values()) {
+            if (handler.handle(m)) {
+                return;
+            }
+        }
+        System.out.println(m + " is a dead letter");
+    }
+
+    public static void main(String[] args) {
+
+        for (Mail mail : Mail.generator(10)) {
+            System.out.println(mail.details());
+            handle(mail);
+            System.out.println("********");
+        }
+    }
+
+
+}
