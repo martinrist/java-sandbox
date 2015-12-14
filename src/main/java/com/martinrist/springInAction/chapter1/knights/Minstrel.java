457d3d754003790146d242f82c140ec000134cf9
Martin Rist <mlr-github@martinrist.com>
2015-12-14 12:17:06 +0000

    Introduce Minstrel as an Aspect for logging.

---
+++ src/main/java/com/martinrist/springInAction/chapter1/knights/Minstrel.java
@@ -0,0 +1,24 @@
+package com.martinrist.springInAction.chapter1.knights;
+
+import java.io.PrintStream;
+
+/**
+ * A medieval logger.
+ */
+public class Minstrel {
+
+    private PrintStream out;
+
+    public Minstrel(PrintStream out) {
+        this.out = out;
+    }
+
+    public void singBeforeQuest() {
+        out.println("Fa la la, the knight is so brave!");
+    }
+
+    public void singAfterQuest() {
+        out.println("Tee hee hee, the brave knight did embark on a quest!");
+    }
+
+}
