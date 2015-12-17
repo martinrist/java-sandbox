67164190345d4f2c06d123b61653c1cde6e3c466
Martin Rist <mlr-github@martinrist.com>
2015-12-17 16:10:39 +0000

    Kill unused imports

--- src/main/java/com/martinrist/springInAction/chapter3/MealConfig.java
+++ src/main/java/com/martinrist/springInAction/chapter3/MealConfig.java
@@ -1,9 +1,7 @@
 package com.martinrist.springInAction.chapter3;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan
