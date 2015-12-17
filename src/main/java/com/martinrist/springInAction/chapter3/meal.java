8b5b74d7ecfb70b5feb60c47ea7be723e41a22ba
Martin Rist <mlr-github@martinrist.com>
2015-12-17 15:54:57 +0000

    Attempt to correct problems with incorrectly-cased meal.java file

--- src/main/java/com/martinrist/springInAction/chapter3/meal.java
+++ src/main/java/com/martinrist/springInAction/chapter3/meal.java
@@ -1,6 +1,7 @@
 package com.martinrist.springInAction.chapter3;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,7 @@ public class Meal {
     private Dessert dessert;
 
     @Autowired
+    @Qualifier("iceCream")
     public void setDessert(Dessert dessert) {
         this.dessert = dessert;
     }
