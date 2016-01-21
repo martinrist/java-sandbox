b4f9bd2d06d9e5d9578287cd6e85d2c9adc35b01
Martin Rist <mlr-github@martinrist.com>
2016-01-21 10:44:38 +0000

    Configure caching for SpittleRepository using Ehcache.  The log messages in DummySpittleRepository can be used to see when we're getting a cache hit vs miss.

--- src/main/java/com/martinrist/springInAction/spittr/data/DummySpittleRepository.java
+++ src/main/java/com/martinrist/springInAction/spittr/data/DummySpittleRepository.java
@@ -1,6 +1,7 @@
 package com.martinrist.springInAction.spittr.data;
 
 import com.martinrist.springInAction.spittr.domain.Spittle;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,9 +11,14 @@ import java.util.List;
 @Repository
 public class DummySpittleRepository implements SpittleRepository {
 
+    static long SPITTLE_ID_COUNTER = 1000;
+
+    private static final Logger LOGGER = Logger.getLogger(DummySpittleRepository.class);
+
     @Override
     public List<Spittle> findSpittles(long max, int count) {
 
+        LOGGER.info(String.format("Finding %d Spittles up to ID %d", max, count));
         List<Spittle> spittles = new ArrayList<>();
         for (int i = 1; i <= count; i++) {
             spittles.add(new Spittle((long)i, "Spittle " + i, new Date()));
@@ -23,6 +29,18 @@ public class DummySpittleRepository implements SpittleRepository {
 
     @Override
     public Spittle findSpittle(long id) {
+        LOGGER.info(String.format("Finding Spittle with ID %d", id));
         return new Spittle(id, "Spittle " + id, new Date());
+    }
+
+    @Override
+    public Spittle save(Spittle spittle) {
+        Spittle result = new Spittle(SPITTLE_ID_COUNTER++, "Spittle " + SPITTLE_ID_COUNTER, new Date());
+        return result;
+    }
+
+    @Override
+    public void remove(long spittleId) {
+        // Do nothing, as this is a dummy implementation
     }
 }
--- src/main/java/com/martinrist/springInAction/spittr/data/SpittleRepository.java
+++ src/main/java/com/martinrist/springInAction/spittr/data/SpittleRepository.java
@@ -1,6 +1,9 @@
 package com.martinrist.springInAction.spittr.data;
 
 import com.martinrist.springInAction.spittr.domain.Spittle;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -8,7 +11,17 @@ import java.util.List;
  * Interface for accessing a repository of Spittles data
  */
 public interface SpittleRepository {
+
+    @Cacheable("spittleCache")
     List<Spittle> findSpittles(long max, int count);
 
+    @Cacheable("spittleCache")
     Spittle findSpittle(long id);
+
+    @CachePut(value = "spittleCache", key = "#result.id")
+    Spittle save(Spittle spittle);
+
+    @CacheEvict("spittleCache")
+    void remove(long spittleId);
+
 }
