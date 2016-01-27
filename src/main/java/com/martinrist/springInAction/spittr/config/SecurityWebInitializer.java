1b4cc6a22c090065a9fcc5766f94bec42b3dad2b
Martin Rist <mlr-github@martinrist.com>
2016-01-27 15:36:35 +0000

    Configure MultipartFilter so that it comes before Spring Security Filter.  Otherwise attempting to submit a CSRF token in the multipart/form-data-encoded registration form doesn't work and causes a 403 error.

--- src/main/java/com/martinrist/springInAction/spittr/config/SecurityWebInitializer.java
+++ src/main/java/com/martinrist/springInAction/spittr/config/SecurityWebInitializer.java
@@ -1,7 +1,14 @@
 package com.martinrist.springInAction.spittr.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
+
+import javax.servlet.ServletContext;
 
 public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
 
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        insertFilters(servletContext, new MultipartFilter());
+    }
 }
