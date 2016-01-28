900bbaef3fabd3a1b713e9f8a4efcb2e0f11b477
Martin Rist <mlr-github@martinrist.com>
2016-01-28 15:03:02 +0000

    Use @RestController in place of @Controller to remove per-method @ResponseBody annotations.

--- src/main/java/com/martinrist/springInAction/spittr/ws/SpittleRESTController.java
+++ src/main/java/com/martinrist/springInAction/spittr/ws/SpittleRESTController.java
@@ -3,13 +3,12 @@ package com.martinrist.springInAction.spittr.ws;
 import com.martinrist.springInAction.spittr.domain.Spittle;
 import com.martinrist.springInAction.spittr.service.SpitterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/spittles")
 public class SpittleRESTController {
 
@@ -21,17 +20,17 @@ public class SpittleRESTController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody List<Spittle> spittles(@RequestParam(value="count", defaultValue = "20") int count) {
+    public List<Spittle> spittles(@RequestParam(value="count", defaultValue = "20") int count) {
         return service.getRecentSpittles(count);
     }
 
     @RequestMapping(path = "/{spittleId}", method = RequestMethod.GET)
-    public @ResponseBody Spittle spittle(@PathVariable("spittleId") long spittleId, Model model) {
+    public Spittle spittle(@PathVariable("spittleId") long spittleId, Model model) {
         return service.getSpittleById(spittleId);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
-    public @ResponseBody Spittle saveSpittle(@RequestBody Spittle spittle) {
+    public Spittle saveSpittle(@RequestBody Spittle spittle) {
         return service.saveSpittle(spittle);
     }
 }
