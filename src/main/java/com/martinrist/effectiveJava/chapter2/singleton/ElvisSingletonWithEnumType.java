7619619913bfb40e40e420204c9c9d1613cf0202
Martin Rist <mlr-github@martinrist.com>
2016-03-17 12:30:59 +0000

    Add example of Singleton implementation using a single-valued enum type.

---
+++ src/main/java/com/martinrist/effectiveJava/chapter2/singleton/ElvisSingletonWithEnumType.java
@@ -0,0 +1,13 @@
+package com.martinrist.effectiveJava.chapter2.singleton;
+
+/**
+ * Implementation of a singleton with a single-valued enum type
+ */
+public enum ElvisSingletonWithEnumType {
+
+    INSTANCE;
+
+    public void leaveTheBuilding() {
+        // ... implementation goes here
+    }
+}
